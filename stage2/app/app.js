5658c450a42cd237c2494c00243e07e7431e3786
Tobias Löb <tloeb@inwerken.de>
2018-10-01 16:04:29 +0200

    ADDED dns rebind protection

--- stage2/app/app.js
+++ stage2/app/app.js
@@ -3,11 +3,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
+var hostValidation = require('host-validation');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Protection against DNS Rebind attacks
+app.use(hostValidation({ hosts: ['127.0.0.1:3000',
+                                 'localhost:3000'] }))
 
 app.use(logger('dev'));
 app.use(express.json());
